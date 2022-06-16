a75884133b1a010bdd1aa1d9900cef70f7b42200
Nikita <nicita.zinkevitch@yandex.ru>
2022-06-16 16:56:51 +0300

    Add map and a loop to fill him

--- langtest.go
+++ langtest.go
@@ -8,15 +8,29 @@ import (
 
 func main() {
 
-	reader := bufio.NewReader(os.Stdin)
+	translations := make(map[string]string)
+	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Println("Welcome to easy vocabulary test utility. Please, append some words with translations below.")
 	for true {
 		fmt.Println("Foreign word")
-		foreign, _, _ := reader.ReadLine()
+		scanner.Scan()
+		var foreign string = scanner.Text()
 
 		fmt.Println("Translation")
-		transl, _, _ := reader.ReadLine()
+		scanner.Scan()
+		var transl string = scanner.Text()
 
-		fmt.Printf("You have chosen foreign word %s and translation %s\n", foreign, transl)
+		if foreign == "" || transl == "" {
+			fmt.Println("Empty input, ignored")
+			continue
+		} else if foreign == "stop" || transl == "stop" {
+			for f, t := range translations {
+				fmt.Printf("Foreign language %s with translation %s\n", f, t)
+			}
+			break
+		}
+
+		translations[foreign] = transl
+
 	}
 }
